bae68bff3b1296f17fd47337f28e16cf7ad71d78
NileshJorwar <jorwarnilesh@gmail.com>
2020-04-03 00:48:40 -0500

    Initial commit

---
+++ src/main/java/com/tddexamples/datastructurestdd/DatastructurestddApplication.java
@@ -0,0 +1,13 @@
+package com.tddexamples.datastructurestdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructurestddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastructurestddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tddexamples/datastructurestdd/DatastructurestddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddexamples.datastructurestdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructurestddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
